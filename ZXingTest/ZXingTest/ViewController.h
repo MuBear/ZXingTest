ca1068f7505cf31510c8683f09b4158c58b0a5ba
jlai <result0924@gmail.com>
2015-10-14 11:45:47 +0800

    First commit and add Podfile

---
+++ ZXingTest/ZXingTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZXingTest
+//
+//  Created by justin on 2015/10/14.
+//  Copyright © 2015年 justin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
