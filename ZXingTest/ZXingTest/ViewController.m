eb9ccd9619083e7c3a71bfbee4e7dd2b665d994f
jlai <result0924@gmail.com>
2015-10-23 10:16:49 +0800

    add decode qrcode from photo

--- ZXingTest/ZXingTest/ViewController.m
+++ ZXingTest/ZXingTest/ViewController.m
@@ -364,8 +364,39 @@
 
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
     UIImage *img = [info objectForKey:@"UIImagePickerControllerOriginalImage"];
-    [self shareLineWithImage:img];
-    [self dismissViewControllerAnimated:YES completion:nil];
+    [self dismissViewControllerAnimated:YES completion:^ {
+        CGImageRef imageToDecode = img.CGImage;  // Given a CGImage in which we are looking for barcodes
+
+        ZXLuminanceSource *source = [[ZXCGImageLuminanceSource alloc] initWithCGImage:imageToDecode];
+        ZXBinaryBitmap *bitmap = [ZXBinaryBitmap binaryBitmapWithBinarizer:[ZXHybridBinarizer binarizerWithSource:source]];
+
+        NSError *error = nil;
+
+        // There are a number of hints we can give to the reader, including
+        // possible formats, allowed lengths, and the string encoding.
+        ZXDecodeHints *hints = [ZXDecodeHints hints];
+
+        ZXMultiFormatReader *reader = [ZXMultiFormatReader reader];
+        ZXResult *result = [reader decode:bitmap
+                                    hints:hints
+                                    error:&error];
+        if (result) {
+            // The coded result as a string. The raw data can be accessed with
+            // result.rawBytes and result.length.
+            NSString *contents = result.text;
+
+            // The barcode format, such as a QR code or UPC-A
+            //        ZXBarcodeFormat format = result.barcodeFormat;
+
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:nil message:contents delegate:self cancelButtonTitle:@"NO" otherButtonTitles:@"YES", nil];
+            [alert show];
+        } else {
+            // Use error to determine why we didn't get a result, such as a barcode
+            // not being found, an invalid checksum, or a format inconsistency.
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:nil message:@"not qrcode image" delegate:self cancelButtonTitle:@"NO" otherButtonTitles:@"YES", nil];
+            [alert show];
+        }
+    }];
 }
 
 #pragma mark - LXActivityDelegate
